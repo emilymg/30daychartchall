81c29094de7e8c80ef2467b6fb098559a6e906a9
emilymg <emilymg@users.noreply.github.com>
2021-04-09 12:21:26 -0400

    update day 7

--- Day7/Day7_code.R
+++ Day7/Day7_code.R
@@ -9,13 +9,8 @@ library(ggstream)
 # Load tidy tuesday data
 tues_15 <- tt_load("2021-04-06")
 
-# Save separate data frames
+# Save Brazil data
 brazil_loss <- tues_15$brazil_loss
-forest <- tues_15$forest
-forest_area <- tues_15$forest_area
-soybean <- tues_15$soybean_use
-veg_oil <- tues_15$vegetable_oil
-
 glimpse(brazil_loss)
 
 # Make a long df
@@ -29,13 +24,16 @@ pal <- colorRamps::matlab.like(11)
 
 # Plot stream
 p_brazil <- ggplot(brazil_long, aes(x = year, y = value, fill = cause)) +
-  geom_stream() +
+  geom_stream(extra_span = 0.01, type = "proportional") +
   scale_x_continuous(breaks = c(2002, 2004, 2006, 2008, 2010, 2012)) +
-  scale_y_continuous("Forest Loss (hectares)", labels = scales::comma) +
   scale_fill_manual(values = pal)
 # Add theme and labels
-p_brazil + theme(panel.background = element_rect(fill = "grey26"),
+p_brazil + 
+  theme(panel.background = element_rect(fill = "white"),
                    panel.grid = element_blank(),
                    legend.title = element_blank()) +
   labs(title = "Causes of Deforestation in Brazil, 2001-2013",
-       caption = "Data Source: Our World in Data, https://ourworldindata.org/palm-oil")
+       caption = "Data Source: #tidytuesday 2021 week 15, Our World in Data, https://ourworldindata.org/palm-oil",
+       x = NULL,
+       y = "Proportion of total forest loss")
+
